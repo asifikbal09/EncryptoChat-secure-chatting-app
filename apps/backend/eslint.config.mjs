5d883ab078152f4979f87fb5a4e02b57414e1e56
asifikbal09 <asif.ikbal662@gmail.com>
2025-04-29 00:53:32 +0600

    Setup backend project with TypeScript, Express, ESLint, Prettier

---
+++ apps/backend/eslint.config.mjs
@@ -0,0 +1,29 @@
+// eslint.config.mjs
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  {
+    ignores: [".node_modules/*"]
+},
+];
+
+module.exports = [
+  // Any other config imports go at the top
+  eslintPluginPrettierRecommended,
+];
